1fc279142a65bb3f5da532b284925beb5cdc4043
Georgetteakinyi <47854444+Georgetteakinyi@users.noreply.github.com>
2019-04-30 19:26:32 +0300

    Add files via upload

---
+++ shapes.py
@@ -0,0 +1,66 @@
+class circle:
+	def __init__(self,radius,area,circumference):
+		self.radius=r
+		self.area=A
+		self.circumference=C
+	def radius_circle(self):
+		r=self.radius_circle
+		print radius
+	def area(self):
+		A= (3.14*(r*r))
+		print  A     
+	def circumference (self):
+		C =(2*3.14*r) 
+		print C
+class square:
+	
+		
+	def __init__(self,side_square,area,perimeter):
+		self.side_square=a
+		self.area=A
+		self.perimeter=P
+	def side_square(self):
+	    a=self.side_square
+	    print a
+	def area(self):
+		A=(r*r)
+		print A
+    def perimeter(self):
+    	P=(4*r)
+    	print P
+class rectangle:
+    def __init__(self,sides,area,perimeter):
+    	self.sides=sides
+    	self.area=A
+    	self.perimeter=P
+    def sides(self):
+        sides=(w,l)
+        print w,l
+
+    def area(self):
+    	A=(w*l)
+    	print A
+    def perimeter(self):
+    	P=2(w+l)
+    	print P
+class sphere:    	
+    def __init__(self,radius,area,volume):
+		self.radius=r
+		self.area=A
+		self.volume=V
+	def radius(self):
+		r=self.radius
+		print radius
+	def area(self):
+		A= (4*3.14*(r*r)
+		print  A 
+
+    def volume(self):
+		V=4/3(3.14*(r*r*r))
+		print V
+
+		
+
+
+		
+	
