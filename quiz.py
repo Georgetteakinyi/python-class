8baa6ca62a95550843f5523a5d1714fce38047cf
Georgetteakinyi <47854444+Georgetteakinyi@users.noreply.github.com>
2019-05-07 19:21:58 +0300

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,44 @@
+tens=[1,10]*10
+print(tens)
+
+squares = [2,4,6,8,10,12]
+ for x in (2,4,6,8,10,12):
+     squares.append(x**2)
+
+
+sorted=([9,1,4,7,3],[5,2,6,8,0])
+sorted=(sorted.sort(sorted))
+print(sorted)
+
+
+range_sum= (n,n+1)
+for (var i =1:i<=n;i++)
+return(range_sum)
+
+ 
+
+
+largest=([1,3,6,8,2,4,5.7]) 
+    print(max(1,3,6,8,2,4,5.7))
+
+
+
+
+smallest=([1,3,6,8,2,4,5.7]) 
+    print(min(1,3,6,2,4,5,7))
+
+
+
+my_modulous=[10,20]
+ for x in range((10,20)/3):
+    print(x)
+
+
+
+
+ students = [{'balance': 1000, 'name': 'Irene'}, {'balance': 2000, 'name': 'Pauline'}, {'balance': 3000, 'name': 'Naima'}, {'balance': 1000, 'name': 'Rose'}]
+
+     for balance, name in students:
+		print("hello {} your balance is {}".format(balnce,name))
+
+
